65f68183c0c934fb9eabdcdebcc0bc4f8bf1f928
jazonthemazon <97111436+jazonthemazon@users.noreply.github.com>
2025-11-06 16:09:03 +0100

    added swing

--- Assets/_Scripts/Music Generator/MusicGenerator.cs
+++ Assets/_Scripts/Music Generator/MusicGenerator.cs
@@ -5,10 +5,13 @@ public class MusicGenerator : MonoBehaviour
 {
     [Header("Metronome")]
     [SerializeField] private bool _metronomeActive;
+    [SerializeField] private AudioData _metronomeAudioOnFirstBeat;
     [SerializeField] private AudioData _metronomeAudio;
     
     [Header("Tempo")]
-    [SerializeField][Min(1)] private double _bpm = 120;
+    [SerializeField][Range(10, 500)] private double _bpm = 120;
+    [SerializeField][Min(1)] private int _timeSignature;
+    [SerializeField][Range(50f, 99f)] private double _swing;
 
     [Header("Instruments")]
     [SerializeField] private List<Instrument> _instruments;
@@ -34,14 +37,21 @@ public class MusicGenerator : MonoBehaviour
         // Play Metronome Audio
         if (_metronomeActive)
         {
-            AudioManager.Instance.CreateAudio(_metronomeAudio).PlayScheduled(_nextStepTime);
+            AudioData metronomeAudio = _stepIndex == 0 ? _metronomeAudioOnFirstBeat : _metronomeAudio;
+            AudioManager.Instance.CreateAudio(metronomeAudio).PlayScheduled(_nextStepTime);
         }
 
         // Advance to next step
-        _stepIndex = (_stepIndex + 1) % 16;
+        _stepIndex = (_stepIndex + 1) % _timeSignature;
 
         // Recalculate interval in case bpm changed
         _stepInterval = 60.0 / _bpm;
+        
+        // calculate swing
+        double stepIntervalWithSwing = _stepInterval * (_swing / 50.0);
+        
+        // add swing to interval
+        _stepInterval = _stepIndex % 2 != 0 ? stepIntervalWithSwing : _stepInterval * 2 - stepIntervalWithSwing;
 
         // Calculate exact timing of next beat
         _nextStepTime = AudioSettings.dspTime + _stepInterval;
